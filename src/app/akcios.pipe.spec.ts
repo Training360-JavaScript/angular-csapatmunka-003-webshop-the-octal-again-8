dd7a5e9beeb14fbdb91f921a6bcecda48dbddda8
Hajni83 <maro.hajnalka@gmail.com>
2022-01-21 11:46:53 +0100

    kiemelt, akcios pipe

---
+++ src/app/akcios.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { AkciosPipe } from './akcios.pipe';
+
+describe('AkciosPipe', () => {
+  it('create an instance', () => {
+    const pipe = new AkciosPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { Cat01Component } from './page/cat01/cat01.component';
 import { Cat02Component } from './page/cat02/cat02.component';
 import { HomeComponent } from './page/home/home.component';
+import { KiemeltPipe } from './kiemelt.pipe';
+import { AkciosPipe } from './akcios.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomeComponent } from './page/home/home.component';
     NavigationComponent,
     Cat01Component,
     Cat02Component,
-    HomeComponent
+    HomeComponent,
+    KiemeltPipe,
+    AkciosPipe
   ],
   imports: [
     BrowserModule,
