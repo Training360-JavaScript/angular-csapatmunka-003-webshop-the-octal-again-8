09544f98778222d63775cfe542a274f7a530928b
barotiv <baroti.viktor@gmail.com>
2022-01-22 16:04:25 +0100

    Product-card without design

--- src/app/common/product-card/product-card.component.ts
+++ src/app/common/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../../model/product.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
+
+  @Input() product: any = Product;
 
   constructor() { }
 
