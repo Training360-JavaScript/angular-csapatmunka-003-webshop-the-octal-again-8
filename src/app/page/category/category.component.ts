c234f76a7303a899c8b2f0709d9d8449f06192c6
Daniel Kovacs <info@kovix.info>
2022-01-22 20:19:38 +0100

    parse routing params in category

--- src/app/page/category/category.component.ts
+++ src/app/page/category/category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from 'src/app/service/category.service';
+import { Category } from 'src/app/model/category.model';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  id: Observable<number> = this.activatedRoute.params.pipe(
+    map(params => params['catId'])
+  );
 
-  constructor() { }
+  catId: number = 0;
+  category: Category = new Category();
+
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private categoryService: CategoryService,
+  ) { }
 
   ngOnInit(): void {
+    this.id.subscribe((catId: number) => {
+      console.log(typeof catId);
+      this.catId = +catId;
+      this.category = this.categoryService.categories.find(el => el.id === this.catId) || new Category();
+      //TODO: Nav to 404 if not found
+    });
   }
 
 }
