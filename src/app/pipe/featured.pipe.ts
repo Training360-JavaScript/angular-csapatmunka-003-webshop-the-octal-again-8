6a74a7cb105e5cb2131d7718f492dd4b7b4f9d01
Daniel Kovacs <info@kovix.info>
2022-01-23 19:43:44 +0100

    merge

--- src/app/pipe/featured.pipe.ts
+++ src/app/pipe/featured.pipe.ts
@@ -7,7 +7,7 @@ import { Product } from '../model/product.model';
 export class FeaturedPipe implements PipeTransform {
 
   transform(value: Product[], arg: number = 5): Product[] {
-    return value.filter((a)=>a.featured).slice(0, arg - 1);
+    return value.filter((a)=>a.featured).slice(0, arg);
   }
 
 }
--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -1,14 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Product } from '../model/product.model';
 
-
 @Pipe({
-  name: 'filter'
+  name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
-
   transform(value: Product[], phrase: string): Product[] {
-    return value.filter((a)=> a.name.toLowerCase().includes(phrase.toLowerCase()) );
+    return value.filter((a) => {
+      let numberPhrase = parseInt(phrase);
+      if (!isNaN(numberPhrase)) {
+        return a.price === numberPhrase;
+      } else {
+        return (
+          a.name.toLowerCase().includes(phrase.toLowerCase()) ||
+          a.description.toLowerCase().includes(phrase.toLowerCase())
+        );
+      }
+    });
   }
-
 }
