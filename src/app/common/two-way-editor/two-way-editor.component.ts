e5d766a74799c3febfd58198da35cbb343f3507e
Hajni83 <maro.hajnalka@gmail.com>
2022-01-31 21:46:02 +0100

    category selector fix, euro symbol fix

--- src/app/common/two-way-editor/two-way-editor.component.ts
+++ src/app/common/two-way-editor/two-way-editor.component.ts
@@ -3,36 +3,35 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Category } from 'src/app/model/category.model';
 import { environment } from '../../../environments/environment';
 
-
 @Component({
   selector: '[app-two-way-editor]',
   templateUrl: './two-way-editor.component.html',
-  styleUrls: ['./two-way-editor.component.scss']
+  styleUrls: ['./two-way-editor.component.scss'],
 })
 export class TwoWayEditorComponent implements OnInit {
+  @Input() product: Product = new Product();
+  @Input() categories!: Category[] | null;
 
+  @Output() delProduct: EventEmitter<Product> = new EventEmitter();
+  @Output() updateProduct: EventEmitter<Product> = new EventEmitter();
 
-   @Input() product :Product = new Product();
-   @Input() categories!: Category[]|null;
+  isEditable: boolean = false;
+  imageBaseURL: string = `${environment.baseURL}product_images/`;
 
-   @Output() delProduct:EventEmitter<Product> = new EventEmitter();
-   @Output() updateProduct:EventEmitter<Product> = new EventEmitter();
+  constructor() {}
 
-   isEditable:boolean = false;
-   imageBaseURL: string = `${environment.baseURL}product_images/`;
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  compareFn(c1: Category, c2: Category): boolean {
+    return c1 && c2 ? c1.id === c2.id : c1 === c2;
   }
 
-  onDelete(product:Product):void {
+  onDelete(product: Product): void {
     this.delProduct.emit(product);
   }
 
-  onUpdate(product:Product):void{
+  onUpdate(product: Product): void {
     this.updateProduct.emit(product);
-    this.isEditable=false;
+    this.isEditable = false;
   }
-
 }
