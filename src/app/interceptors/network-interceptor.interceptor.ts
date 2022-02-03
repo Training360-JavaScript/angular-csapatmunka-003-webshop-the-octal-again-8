5a2808803d7b5f1bdf845ca02167498bb366e33f
Daniel Kovacs <info@kovix.info>
2022-02-03 08:13:52 +0100

    Add very basic error handling. (just an alert)

---
+++ src/app/interceptors/network-interceptor.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable()
+export class NetworkInterceptorInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+
+        const messageToShow = `Network error\n${errorMessage}\n\nDoes JSON server running? Plese enter _____JSON_SERVER folder, then npm i && node app.js`;
+        window.alert(messageToShow);
+
+        return throwError(errorMessage);
+
+      })
+
+    )
+  }
+}
